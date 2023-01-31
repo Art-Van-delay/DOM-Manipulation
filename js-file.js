3f79143fb2c9142cc3d158fab26c76313c890edc
Art <mnsenna@protonmail.com>
2023-01-31 15:41:19 +1000

    Complete dom manipulation walkthrough'

--- js-file.js
+++ js-file.js
@@ -39,7 +39,24 @@ divTwo.appendChild(headOne);
 
 const paraTwo = document.createElement('p');
 paraTwo.textContent = 'ME TOO!';
-
 divTwo.appendChild(paraTwo);
+// method 2
+// const btn = document.querySelector('#btn');
+// btn.onclick = () => alert('Hello')
 
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', function (e) {
+    console.log(e.target);
+    e.target.style.background = 'blue';
+});
 
+// buttons is a node list. It looks and acts much like an array
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+    // and for each one we add a click listener
+    button.addEventListener('click', () => {
+        alert(button.id);
+    });
+});
